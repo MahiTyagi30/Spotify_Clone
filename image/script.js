ac065b0a08ffa4bd5539927d0e78b6fee602edb4
Mahi Tyagi <mahityagi2003@gmail.com>
2022-12-29 15:20:26 +0530

    song cover changed

--- image/script.js
+++ image/script.js
@@ -10,15 +10,15 @@ let mastersongname=document.getElementById('mastersongname');
 let songItem=Array.from(document.getElementsByClassName('songItems'));
 
 let song =[
-   { songName: "BOLNA HALKE HALKE",filePath: "song/1.mp3",coverPath:"image/cover1.jpeg"},
-   { songName: "PHLE NAZAR",filePath: "song/2.mp3",coverPath:"image/cover1.jpeg"},
-   { songName: "KAISE HUA",filePath: "song/3.mp3",coverPath:"image/cover1.jpeg"},
-   { songName: "MAAN MERI JAAN",filePath: "song/4.mp3",coverPath:"image/cover1.jpeg"},
-   { songName: "RAATAN LABIYA",filePath: "song/5.mp3",coverPath:"image/cover1.jpeg"}
+   { songName: "BOLNA HALKE HALKE",filePath: "song/1.mp3",coverPath:"image/bolnahalke.jpeg"},
+   { songName: "PHLE NAZAR",filePath: "song/2.mp3",coverPath:"image/phlenazar.jpeg"},
+   { songName: "KAISE HUA",filePath: "song/3.mp3",coverPath:"image/kaisehua.jpeg"},
+   { songName: "MAAN MERI JAAN",filePath: "song/4.mp3",coverPath:"image/maanmerijaan.jpeg"},
+   { songName: "RAATAN LAMBIYA",filePath: "song/5.mp3",coverPath:"image/raatanlambiya.jpeg"}
 ]
 songItem.forEach((element,i)=>{
     element.getElementsByTagName("img")[0].src=song[i].coverPath;
-    element.getElementsByClassName("songname")[0].innerText=song[i].songName;
+    element.getElementsByClassName("songname")[0].innerText=song[i].songName;//let me  love you is changed to song[] name
 })
 
 
@@ -43,20 +43,19 @@ audioElement.addEventListener('timeupdate',()=>{
     //update seek bar
     progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
     
-    myProgressRange.vaule=progress
+    myProgressRange.value=progress
 })
 myProgressRange.addEventListener('change',()=>{
-    audioElement.currentTime=myProgressRange.vaule*audioElement.duration/100;
+    audioElement.currentTime=myProgressRange.value*audioElement.duration/100;
 })
 const makeAllPlays = ()=>{
     
     Array.from(document.getElementsByClassName('songitemPlay')).forEach((element)=>{
         element.classList.remove('fa-pause');
         element.classList.add('fa-play');
-
-
 })
 }
+
 
 Array.from(document.getElementsByClassName('songitemPlay')).forEach((element)=>{
     element.addEventListener('click',(e)=>{
@@ -73,11 +72,42 @@ Array.from(document.getElementsByClassName('songitemPlay')).forEach((element)=>{
         gif.style.opacity=1;
         masterPlay.classList.remove('fa-play');
         masterPlay.classList.add('fa-pause');
-
-
     })
-
 })
+//  const makeAllPause=()=>{
+//     Array.from(document.getElementsByClassName('songitemPlay')).forEach((element)=>{
+//         element.classList.remove('fa-play');
+//         element.classList.add('fa-pause');
+
+
+// })
+
+//  }
+
+// Array.from(document.getElementsByClassName('songitemPlay')).forEach((element)=>{
+//     element.addEventListener('click',(e)=>{
+//         console.log(e.target);
+//         makeAllPause();
+        
+//         songIndex=parseInt(e.target.id);
+//         e.target.classList.remove('fa-pause');
+//         e.target.classList.add('fa-play');
+//         audioElement.src=`song/${songIndex+1}.mp3`;
+//         mastersongname.innerText=song[songIndex].songName;
+//         audioElement.currentTime=myProgressRange.value;
+//         audioElement.play();
+//         gif.style.opacity=0;
+//         masterPlay.classList.remove('fa-pause');
+//         masterPlay.classList.add('fa-play');
+
+
+//     })
+
+// })
+
+
+
+
 document.getElementById('next').addEventListener('click',()=>{
     if(songIndex>=4){
         songIndex=0;
